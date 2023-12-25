e4c60b848ff366d4b949cf42db8b9a1c7ab48a78
Haithem <haithem.bouallagui90@gmail.com>
2023-12-25 23:44:22 +0100

    update test

--- test_selenium.py
+++ test_selenium.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-# Set up the WebDriver (in this case, using Firefox)
-driver = webdriver.Firefox()
+# Set up Chrome options for headless mode
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')  # Disable GPU acceleration for headless mode
+
+# Set up the WebDriver (in this case, using Chrome in headless mode)
+driver = webdriver.Chrome(options=chrome_options)
 
 try:
     # Open the webpage
-    driver.get("file:///path/to/your/local/project/index.html")
+    driver.get("http://35.180.87.169/index.html")
 
-    # Get the title of the page
+    # Get the title of the webpage
     title = driver.title
 
-    # Check if the title matches the expected value
     expected_title = "Haithem Ansible Pipeline"
     assert title == expected_title, f"Expected title: {expected_title}, Actual title: {title}"
 
